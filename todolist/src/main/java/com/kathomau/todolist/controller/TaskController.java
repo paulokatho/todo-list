74cabf2dbf5b7f2e860d8ffe23756fbec9d466a1
paulo-pay <paulo.fernandes@payshopx.com.br>
2021-12-11 08:22:48 -0300

    Implementing the create, listAll and findById methods.

--- todolist/src/main/java/com/kathomau/todolist/controller/TaskController.java
+++ todolist/src/main/java/com/kathomau/todolist/controller/TaskController.java
@@ -1,5 +1,44 @@
 package com.kathomau.todolist.controller;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.kathomau.todolist.model.Task;
+import com.kathomau.todolist.service.TaskService;
+
+@RestController
+@RequestMapping("/api/v1")
 public class TaskController {
 
+	@Autowired
+	private TaskService taskService;
+	
+	@PostMapping("/tasks")
+	@ResponseStatus(HttpStatus.CREATED)
+	public Task createTask(@RequestBody Task task) {
+		return taskService.createTask(task);
+	}
+	
+	@GetMapping("/tasks")
+	@ResponseStatus(HttpStatus.OK)
+	public List<Task> getAllTasks() {
+		return taskService.listAllTasks();
+	}
+	
+	@GetMapping("/tasks/{id}")
+	@ResponseStatus(HttpStatus.OK)
+	public ResponseEntity<Task> getTaskById(@PathVariable(value = "id") Long id) {
+		return taskService.findTaskById(id);
+	}
+	
 }
--- todolist/src/main/java/com/kathomau/todolist/service/TaskService.java
+++ todolist/src/main/java/com/kathomau/todolist/service/TaskService.java
@@ -1,39 +1,34 @@
 package com.kathomau.todolist.service;
 
-import java.util.Date;
+import java.util.List;
 
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
-import javax.persistence.Table;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
 
-import org.hibernate.annotations.CreationTimestamp;
-import org.hibernate.annotations.UpdateTimestamp;
+import com.kathomau.todolist.model.Task;
+import com.kathomau.todolist.repository.TaskRepository;
 
-import lombok.Getter;
-import lombok.Setter;
+import lombok.RequiredArgsConstructor;
 
-@Entity
-@Table(name = "tasks")
-@Getter
-@Setter
+@Service
+@RequiredArgsConstructor
 public class TaskService {
 
-	@Id
-	@GeneratedValue(strategy = GenerationType.IDENTITY)
-	private Long id;
-	@Column(nullable = false)
-	private String title;
-	@Column(nullable = false)
-	private String description;
-	@Column(nullable = false)
-	private Date deadLine;
-	@CreationTimestamp
-	@Column(name = "created_at", nullable = false, updatable = false)
-	private Date createdAt;
-	@UpdateTimestamp
-	@Column(name = "updated_at")
-	private Date updatedAt;
+	@Autowired
+	private TaskRepository taskRepository;
+	
+	public Task createTask(Task task) {
+		return taskRepository.save(task);
+	}
+	
+	public List<Task> listAllTasks() {
+		return taskRepository.findAll();
+	}
+	
+	public ResponseEntity<Task> findTaskById(Long id) {
+		return taskRepository.findById(id)
+				.map(task -> ResponseEntity.ok().body(task))
+				.orElse(ResponseEntity.notFound().build());
+	}
 }
